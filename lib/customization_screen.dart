ac4129d31596613b35270c8fa46089b646e250fb
aldriangrld <aldriangrld@gmail.com>
2025-10-05 16:42:58 +0800

    Add Instruction 8: Date and time picker inputs in customization form

--- lib/customization_screen.dart
+++ lib/customization_screen.dart
@@ -10,6 +10,36 @@ class _CustomizationScreenState extends State<CustomizationScreen> {
   String _engravingText = '';
   bool _giftBox = false;
   bool _expressDelivery = false;
+  DateTime? _selectedDate;
+  TimeOfDay? _selectedTime;
+
+  // Function to show date picker
+  Future<void> _pickDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2026),
+    );
+    if (picked != null && picked != _selectedDate) {
+      setState(() {
+        _selectedDate = picked;
+      });
+    }
+  }
+
+  // Function to show time picker
+  Future<void> _pickTime(BuildContext context) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+    );
+    if (picked != null && picked != _selectedTime) {
+      setState(() {
+        _selectedTime = picked;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -66,14 +96,51 @@ class _CustomizationScreenState extends State<CustomizationScreen> {
                 ],
               ),
               SizedBox(height: 16.0),
+              Row(
+                children: [
+                  Text(
+                    _selectedDate == null
+                        ? 'Select Delivery Date'
+                        : 'Date: ${_selectedDate!.toString().split(' ')[0]}',
+                  ),
+                  SizedBox(width: 8.0),
+                  ElevatedButton(
+                    onPressed: () => _pickDate(context),
+                    child: Text('Pick Date'),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16.0),
+              Row(
+                children: [
+                  Text(
+                    _selectedTime == null
+                        ? 'Select Delivery Time'
+                        : 'Time: ${_selectedTime!.format(context)}',
+                  ),
+                  SizedBox(width: 8.0),
+                  ElevatedButton(
+                    onPressed: () => _pickTime(context),
+                    child: Text('Pick Time'),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16.0),
               ElevatedButton(
                 onPressed: () {
                   if (_formKey.currentState!.validate()) {
                     _formKey.currentState!.save();
+                    String dateStr = _selectedDate == null
+                        ? 'Not selected'
+                        : _selectedDate!.toString().split(' ')[0];
+                    String timeStr = _selectedTime == null
+                        ? 'Not selected'
+                        : _selectedTime!.format(context);
                     ScaffoldMessenger.of(context).showSnackBar(
                       SnackBar(
                         content: Text(
-                          'Engraving: $_engravingText, Gift Box: $_giftBox, Express Delivery: $_expressDelivery',
+                          'Engraving: $_engravingText, Gift Box: $_giftBox, '
+                          'Express Delivery: $_expressDelivery, Date: $dateStr, Time: $timeStr',
                         ),
                       ),
                     );
